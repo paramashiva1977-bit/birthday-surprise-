32ae2226819fc9b442a46e9637bee2e3ddf1e799
paramashiva1977-bit <paramashiva1977@gmail.com>
2026-07-13 12:23:25 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,22 @@
+function nextPage() {
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("passwordPage").style.display = "block";
+}
+
+function checkPassword() {
+  let pass = document.getElementById("password").value.toLowerCase();
+
+  if (pass === "sijo") {
+    document.getElementById("passwordPage").style.display = "none";
+    document.getElementById("loadingPage").style.display = "block";
+
+    setTimeout(function () {
+      document.getElementById("loadingPage").style.display = "none";
+      document.getElementById("successPage").style.display = "block";
+    }, 3000);
+
+  } else {
+    document.getElementById("wrong").innerHTML =
+      "❌ Oops... that's not the key to my heart ❤️";
+  }
+}
