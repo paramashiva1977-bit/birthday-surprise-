7bceb90ce18be2b5f18e62d9127ed7f1af3f89ca
paramashiva1977-bit <paramashiva1977@gmail.com>
2026-07-13 23:38:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function nextPage() {
-  document.getElementById("bgMusic").play();
   document.getElementById("welcome").style.display = "none";
   document.getElementById("passwordPage").style.display = "block";
 }
