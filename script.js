19fb037e51bf7a24c317ed647760f7d074bbb4f2
paramashiva1977-bit <paramashiva1977@gmail.com>
2026-07-14 00:21:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-
- function nextPage() {
+function nextPage() {
     const music = document.getElementById("bgMusic");
-    music.currentTime = 0;
-    music.play();
+    if (music) {
+        music.currentTime = 0;
+        music.play().catch(() => {});
+    }
 
     document.getElementById("welcome").style.display = "none";
     document.getElementById("passwordPage").style.display = "block";
- }
-
+}
 
 function checkPassword() {
-  let pass = document.getElementById("password").value.toLowerCase();
+    let pass = document.getElementById("password").value.toLowerCase();
 
-  if (pass === "sijo") {
-    document.getElementById("passwordPage").style.display = "none";
-    document.getElementById("loadingPage").style.display = "block";
+    if (pass === "sijo") {
+        document.getElementById("passwordPage").style.display = "none";
+        document.getElementById("loadingPage").style.display = "block";
 
-    setTimeout(function () {
-    document.getElementById("loadingPage").style.display = "none";
-     document.getElementById("letterPage").style.display = "block";
-      
-    }, 3000);
-
-  } else {
-    document.getElementById("wrong").innerHTML =
-      "❌ Oops... that's not the key to my heart ❤️";
-  }
+        setTimeout(function () {
+            document.getElementById("loadingPage").style.display = "none";
+            document.getElementById("letterPage").style.display = "block";
+        }, 3000);
+    } else {
+        document.getElementById("wrong").innerHTML =
+            "❌ Oops... that's not the key to my heart ❤️";
+    }
 }
+
 function showFinal() {
     document.getElementById("letterPage").style.display = "none";
     document.getElementById("finalPage").style.display = "block";
 }
+
 setInterval(function () {
     let heart = document.createElement("div");
     heart.className = "heart";
